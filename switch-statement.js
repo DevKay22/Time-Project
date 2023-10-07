d85dd0dca5fe42179e2a25ddc43bd7e887632b6b
[Kayode Ajala] <[drkay4sure@gmail.com]>
2023-10-07 22:34:32 +0100

    notes on conditional statements

---
+++ switch-statement.js
@@ -0,0 +1,15 @@
+var msg;
+var level = 2;
+
+switch (level) {
+    case 1:
+        msg = "Good luck on the final test";
+        break;
+
+    case 2:
+        msg = "Try again";
+        break;
+
+    default:
+        msg = "Something bad happened";
+}
